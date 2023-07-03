8fa369fd13c6316abca2f709f863d8ae6823a1ce
Danish_khan <danishkhan25nov@gmail.com>
2023-07-03 11:06:32 +0530

    added scroll color

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LexCoin from "./pages/lex-coin/LexCoin";
 import XBtcCoin from "./pages/xbtc-coin/XBtcCoin";
+import "./App.scss";
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/lex" element={<LexCoin />} />
